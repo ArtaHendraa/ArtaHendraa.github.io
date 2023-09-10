367af7ebc37403485f4ba87d4c21f7ae7560181b
Arta Hendraa <artahenda@gmail.com>
2023-09-10 18:31:15 +0800

    2 commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/html/**/*.html", "./public/js/**/*.js"],
+  content: [
+    "./public/**/*.html",
+    "./public/html/**/*.html",
+    "./public/js/**/*.js",
+  ],
   theme: {
     extend: {},
     fontFamily: {
