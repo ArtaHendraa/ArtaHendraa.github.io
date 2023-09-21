3e3ad320d12d3cc021b81c5ea0ed566ca0f6696a
Arta Hendraa <artahenda@gmail.com>
2023-09-21 21:28:33 +0800

    message system update

--- public/js/main.js
+++ public/js/main.js
@@ -72,8 +72,41 @@ closeMenuBtn.addEventListener("click", () => {
 });
 //! Menu Function End
 
-// const messageButton = document.getElementById("message-btn");
+//! message system start
+const scriptURL =
+  "https://script.google.com/macros/s/AKfycbzRsm9eqhKFh8luEkcRHjYvkPKDVRq7n98ECuc6l8XVerSVpEqXiXukBOBVMFkf52lk-g/exec";
+const form = document.forms["artahendraa-contact-form"];
 
-// messageButton.addEventListener("click", () => {
-//   alert("Pesan berhasil terkirim");
-// });
+const btnSubmit = document.querySelector("#send-button");
+const btnLoading = document.querySelector("#loading-btn");
+const alertMassage = document.querySelector(".alert");
+const alertFiledMassage = document.querySelector(".alert-filed");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  btnLoading.classList.toggle("hidden");
+  btnSubmit.classList.toggle("hidden");
+
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) => {
+      console.log("Success!", response);
+      btnLoading.classList.toggle("hidden");
+      btnSubmit.classList.toggle("hidden");
+      alertMassage.classList.toggle("hidden");
+      form.reset();
+    })
+    .catch((error) => {
+      console.error("Error!", error.message);
+      alertFiledMassage.classList.toggle("hidden");
+    });
+});
+
+const closeButtons = document.querySelectorAll(".close");
+
+closeButtons.forEach((closeButton) => {
+  closeButton.addEventListener("click", () => {
+    alertMassage.classList.toggle("hidden");
+  });
+});
+//! message system end
